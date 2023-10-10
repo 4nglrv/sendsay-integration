de047a2bc0620c1a485d6dbed3bfa3e680cbbed5
Oleg Angulyarov <4nglrv@gmail.com>
2023-10-10 20:12:09 +0300

    Some changes

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,4 @@
-git pull origin master
-git checkout master
 cp .env.example .env
 
 echo "Starting prod front container..."
-docker compose --env-file .env -f docker-compose.yml up prod-cap-email-back --build -d || echo "Error occurred when starting front container"
+docker compose --env-file .env -f docker-compose.yml up sendsay-integration --build -d || echo "Error occurred when starting backend container"
